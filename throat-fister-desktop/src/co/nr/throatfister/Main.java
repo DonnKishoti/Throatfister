252ee3a915573481794c0f1efb53e95df1e1edde
Midori Ryuu <Ryuu.Midori@gmail.com>
2012-11-25 11:53:27 +0200

    V0.0002 - Isometric engage!

--- throat-fister-desktop/src/co/nr/throatfister/Main.java
+++ throat-fister-desktop/src/co/nr/throatfister/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "throat-fister";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 960;
+		cfg.height = 640;
 		
 		new LwjglApplication(new ThroatfisterGame(), cfg);
 	}
--- throat-fister/src/co/nr/throatfister/ThroatfisterGame.java
+++ throat-fister/src/co/nr/throatfister/ThroatfisterGame.java
@@ -2,6 +2,7 @@ package co.nr.throatfister;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,30 +10,42 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Plane;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
 
-public class ThroatfisterGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
+public class ThroatfisterGame implements ApplicationListener, InputProcessor {
+	Texture texture;
+	OrthographicCamera cam;
+	SpriteBatch batch;
+	final Sprite[][] sprites = new Sprite[10][10];
+	final Matrix4 matrix = new Matrix4();
+
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+	public void create() {
+		texture = new Texture(Gdx.files.internal("data/rocks.png"));
+		cam = new OrthographicCamera(10,
+				10 * (Gdx.graphics.getHeight() / (float) Gdx.graphics
+						.getWidth()));
+		cam.position.set(5, 5, 10);
+		cam.direction.set(-1, -1, -1);
+		cam.near = 1;
+		cam.far = 100;
+		matrix.setToRotation(new Vector3(1, 0, 0), 90);
+
+		for (int z = 0; z < 10; z++) {
+			for (int x = 0; x < 10; x++) {
+				sprites[x][z] = new Sprite(texture);
+				sprites[x][z].setPosition(x, z);
+				sprites[x][z].setSize(1, 1);
+			}
+		}
+
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -42,14 +55,66 @@ public class ThroatfisterGame implements ApplicationListener {
 	}
 
 	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+	public void render() {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
+		cam.update();
+
+		batch.setProjectionMatrix(cam.combined);
+		batch.setTransformMatrix(matrix);
 		batch.begin();
-		sprite.draw(batch);
+		for (int z = 0; z < 10; z++) {
+			for (int x = 0; x < 10; x++) {
+				sprites[x][z].draw(batch);
+			}
+		}
 		batch.end();
+
+		checkTileTouched();
+	}
+
+	final Plane xzPlane = new Plane(new Vector3(0, 1, 0), 0);
+	final Vector3 intersection = new Vector3();
+	Sprite lastSelectedTile = null;
+
+	private void checkTileTouched() {
+		if (Gdx.input.justTouched()) {
+			Ray pickRay = cam.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+			Intersector.intersectRayPlane(pickRay, xzPlane, intersection);
+			int x = (int) intersection.x;
+			int z = (int) intersection.z;
+			if (x >= 0 && x < 10 && z >= 0 && z < 10) {
+				if (lastSelectedTile != null)
+					lastSelectedTile.setColor(1, 1, 1, 1);
+				Sprite sprite = sprites[x][z];
+				sprite.setColor(1, 0, 0, 1);
+				lastSelectedTile = sprite;
+			}
+		}
+	}
+
+	final Vector3 curr = new Vector3();
+	final Vector3 last = new Vector3(-1, -1, -1);
+	final Vector3 delta = new Vector3();
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		Ray pickRay = cam.getPickRay(x, y);
+		Intersector.intersectRayPlane(pickRay, xzPlane, curr);
+
+		if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+			pickRay = cam.getPickRay(last.x, last.y);
+			Intersector.intersectRayPlane(pickRay, xzPlane, delta);
+			delta.sub(curr);
+			cam.position.add(delta.x, delta.y, delta.z);
+		}
+		last.set(x, y, 0);
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		last.set(-1, -1, -1);
+		return false;
 	}
 
 	@Override
@@ -62,5 +127,41 @@ public class ThroatfisterGame implements ApplicationListener {
 
 	@Override
 	public void resume() {
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
